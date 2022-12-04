d3429e8ba6547ff58eb02edef5d210e2bd5ed271
Artem <artem-golev@list.ru>
2022-12-05 01:56:35 +0300

    "creat fuck version"

--- lab2.5.c
+++ lab2.5.c
@@ -1,3 +1,29 @@
-//
-// Created by Артем ( ͡° ͜ʖ ͡°)  Голев on 07.11.2022.
-//
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int N, i = -1;
+    scanf("%i", &N);
+    int matrix[100 * 100] = { 0 };
+    while (++i < N)
+        scanf("%i", matrix + i);
+
+    int j = 1;
+    for (int n_str = 1; n_str < N; ++n_str) {
+        for (int n_clm = 0; n_clm < N - j; ++n_clm) {
+            int i = N * n_str + n_clm;
+            *(matrix + i) = *(matrix + i - N) + *(matrix + i - N + 1);
+        }
+        ++j;
+    }
+
+    for (int n_str = 0; n_str < N; ++n_str) {
+        for (int n_clm = 0; n_clm < N; ++n_clm) {
+            int i = N * n_str + n_clm;
+            printf("%i ", *(matrix + i));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
