817bbd4d23b2a2537d737893c3b353a78d268dac
Artem <artem-golev@list.ru>
2022-12-11 21:28:15 +0300

    " "

--- lab2.5.c
+++ lab2.5.c
@@ -1,29 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-    int N, i = -1;
-    scanf("%i", &N);
-    int matrix[100 * 100] = { 0 };
-    while (++i < N)
-        scanf("%i", matrix + i);
+#define MAX_SIZE 101
 
-    int j = 1;
-    for (int n_str = 1; n_str < N; ++n_str) {
-        for (int n_clm = 0; n_clm < N - j; ++n_clm) {
-            int i = N * n_str + n_clm;
-            *(matrix + i) = *(matrix + i - N) + *(matrix + i - N + 1);
-        }
-        ++j;
+int main()
+{
+    int matrix[MAX_SIZE*MAX_SIZE] = {0};
+    int N;
+    scanf("%d", &N);
+   // if (N <= 0 ||  N >= MAX_SIZE){
+     //   return 1;
+    //}
+
+    for(int i = 0; i < N; i++) {
+        scanf("%d", matrix + i);
     }
 
-    for (int n_str = 0; n_str < N; ++n_str) {
-        for (int n_clm = 0; n_clm < N; ++n_clm) {
-            int i = N * n_str + n_clm;
-            printf("%i ", *(matrix + i));
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < N - i; j++) {
+            *(matrix + j + i*N) = *(matrix + j + (i-1)*N) + *(matrix + (j+1) + (i-1)*N) ;
+
+        }
+    }
+
+    for(int i = 0;i <N;i++){
+        for(int j = 0; j < N;j++){
+            printf("%d ", *(matrix + j +i*N));
         }
         printf("\n");
     }
-
     return 0;
 }
